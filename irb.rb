a0eddaeca1c08eaaf834e38b812f5d2348e12342
Miguel Barcos <miguel@galar.com>
2010-12-18 08:52:10 -0500

    using hirb

--- irb.rb
+++ irb.rb
@@ -1,6 +1,7 @@
 say_recipe 'irb'
 
-gem 'wirble', :group => :development
-gem 'awesome_print', :require => 'ap', :group => :developemnt
-gem 'interactive_editor', :group => :development
+gem 'wirble', :group => :development 
+gem 'hirb', :group => :development
+# gem 'awesome_print', :require => 'ap', :group => :developemnt
+# gem 'interactive_editor', :group => :development
 
